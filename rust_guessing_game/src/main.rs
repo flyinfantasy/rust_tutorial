beb61f5e6b97f493dc0442e419bfb0ae432eb1ee
FlyInFantasy <felix.kuo.1211@gmail.com>
2024-02-15 13:50:07 +0800

    rust rust_guessing_game

---
+++ rust_guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main(){
+    println!("Please guess a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number }");
+    loop {
+        println!("Guess a number:");
+    
+        let mut guess = String::new(); 
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) =>continue,
+        };
+        
+        println!("You guess {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You get it!");
+                break;
+            },        
+        }
+    }
+
+}
